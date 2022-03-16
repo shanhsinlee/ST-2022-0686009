42d5b98b61306724c10e35245db29cfbaf5b4bcb
shlee <shanhsinlee@gmail.com>
2022-03-16 17:38:26 +0800

    test CI with correct stream answers

--- src/test/java/PriorityQueueTest.java
+++ src/test/java/PriorityQueueTest.java
@@ -10,13 +10,13 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class PriorityQueueTest {
     static Stream<Arguments> streamProvider() {
-        // wrong answers
+        // right answers
         return Stream.of(
-                Arguments.of(new int[]{5, 4, 3, 2}, new int[]{2, 3, 4, 5+1}),
-                Arguments.of(new int[]{5, 4, 3}, new int[]{3, 4, 5+1}),
-                Arguments.of(new int[]{5, 14, 3, 1}, new int[]{1, 3, 5, 14+1}),
-                Arguments.of(new int[]{1, 2}, new int[]{1, 2+1}),
-                Arguments.of(new int[]{}, new int[]{1})
+                Arguments.of(new int[]{5, 4, 3, 2}, new int[]{2, 3, 4, 5}),
+                Arguments.of(new int[]{5, 4, 3}, new int[]{3, 4, 5}),
+                Arguments.of(new int[]{5, 14, 3, 1}, new int[]{1, 3, 5, 14}),
+                Arguments.of(new int[]{1, 2}, new int[]{1, 2}),
+                Arguments.of(new int[]{}, new int[]{})
         );
     }
 
